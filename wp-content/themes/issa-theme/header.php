1363e27ef68dab4d84d7928287794a14525f5a1a
IssaDia <issadiapro@gmail.com>
2020-06-05 20:36:00 +0200

    About-me section ok

--- wp-content/themes/issa-theme/header.php
+++ wp-content/themes/issa-theme/header.php
@@ -1,14 +1,13 @@
 <?php wp_head();
 ?>
-<meta charset="<?php bloginfo('charset'); ?>">
+<meta charset="<?php bloginfo( 'charset' ); ?>">
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Norican&family=Pacifico&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <title>Rising Up</title>
     <?php wp_head() ?>
 </head>
-
 <body>
--- wp-content/themes/issa-theme/template-parts/homepage.php
+++ wp-content/themes/issa-theme/template-parts/homepage.php
@@ -1,19 +1,50 @@
 <div class="container-fluid">
+    <div class=" aboutme-container">
+        <div class="row">
+            <div class="col-md-12 title">
+                <h2>A propos de moi</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-7 bio">
+                <h3>Mathieu Corman</h3>
+                <p>Parcours :</p>
+                <p>Diplôme d'état de masseur kinésithérapeute
+                    Ancien Salarié du centre de rééducation Hélène Borel spécialisé en neurologie notamment dans la prise
+                    en charge des sclérose en plaques et des AVC
+                    Titulaire d'un cabinet libéral composé de 3 masseurs kinésithérapeutes et 2 infirmières.</p>
+                <p>Compétences :</p>
+                <p>Counterstrain, Périnatal, Nourrissons , Trigger points, Nutrition, Vaccuothérapie .</p>
+                <p>Vision :</p> 
+                <p>Emerveillé par l'anatomie et la physiologie du corps humain avec son fonctionnement si harmonieux qui
+                    ne laisse rien au hasard,
+                    Lassé par les résultats sur le long terme des traitements symptomatiques et de la médecine à vision
+                    fragmentée et sectarisée,
+                    D'un tempérament curieux et toujours en recherche de meilleurs résultats, c'est naturellement que je me
+                    suis orienté vers des thérapies qui prennent en compte <strong>la globalité de la personne</strong> : approche multi-
+                    système, mode de vie, environnement..</p>
+            </div>
+            <div class="col-md-5">
+                <div class="aboutme-pic-container">
+                    <img class='aboutme-pic' src="http://localhost/mattproject/wp-content/uploads/2020/06/profilMathieu-scaled.jpg" alt="risingup-creator">
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="row">
-        <div class="col-md-12 card-section-title">
-            <h2><span>Prestations</span></h2>
+        <div class="col-md-12 title">
+            <h2>Services</h2>
         </div>
     </div>
     <div class="cards-list">
         <div class="card-container">
             <a style="display:block" href="/">
                 <div class="card_image">
-                    <img src="https://i.redd.it/b3esnz5ra34y.jpg" />
-
+                    <?php echo get_the_post_thumbnail(13, 'thumbnail'); ?>
                 </div>
             </a>
             <div class="card_title">
-                <p>Card Title</p>
+                <p><?php echo get_the_title(13); ?></p>
             </div>
         </div>
         <div class="card-container">
@@ -21,8 +52,18 @@
                 <img src="https://i.redd.it/b3esnz5ra34y.jpg" />
             </div>
             <div class="card_title">
-                <p>Card Title</p>
+                <p><?php echo get_the_title(14); ?></p>
+            </div>
+        </div>
+        <div class="card-container">
+            <div class="card_image">
+                <img src="https://i.redd.it/b3esnz5ra34y.jpg" />
+            </div>
+            <div class="card_title">
+                <p><?php echo get_the_title(15); ?></p>
             </div>
         </div>
     </div>
 </div>
+
+<?php echo get_the_title(13); ?>
