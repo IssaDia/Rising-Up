dce558220352a485022395998136f1b5151d116c
IssaDia <issadiapro@gmail.com>
2020-06-24 11:34:13 +0200

    Finishing Service section

--- wp-content/themes/issa-theme/section-parts/services-section.php
+++ wp-content/themes/issa-theme/section-parts/services-section.php
@@ -11,23 +11,23 @@ $services_custom_type_obj = get_post_type_object('services');
 $title = $services_custom_type_obj->label;
 $services = new WP_Query($args);
 
+
 ?>
-<div class="col-md-12 title">
-    <span class='title-text'><?php echo $title; ?></span>
-</div>
+
 <?php if ($services->have_posts()) : while ($services->have_posts()) : $services->the_post(); ?>
-        <div class="col-md-6 card-container">
-            <div class="card-service">
-                <a style="display:block" href="<?php the_permalink(); ?>">
-                    <div class="card_image">
-                        <?php echo wp_get_attachment_image(get_post_thumbnail_id(),"", array("class" => "img-responsive")); ?>
-                    </div>
-                </a>
-               
+        <div class="col-md-6 service-container">
+            <div class="service-title">
+                <span><?php the_title(); ?></span>
             </div>
-            <div class="card_title">
-                    <h2><?php the_title(); ?></h2>
-                </div>
+            <div class="service-slogan">
+                <p><?php echo get_field('slogan_texte'); ?></p>
+            </div>
+
+            <div class="service-link">
+                <a href="<?php the_permalink(); ?>">
+                    <h4>Découvrir</h4>
+                </a>
+            </div>
         </div>
     <?php endwhile; ?>
 
