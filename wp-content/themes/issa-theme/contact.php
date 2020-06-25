89fe67ad5b328c8e929b173dd1e5310b261be159
IssaDia <issadiapro@gmail.com>
2020-06-25 23:24:52 +0200

    Adjusting Contact Page Adress

--- wp-content/themes/issa-theme/contact.php
+++ wp-content/themes/issa-theme/contact.php
@@ -4,7 +4,7 @@
  * Template Name: Contact
  */
 get_header();
-$contact_items=get_field('contact');
+$contact_items = get_field('contact');
 $map_url =  get_field('contact')['map'];
 $adress =  get_field('contact')['adress'];
 ?>
@@ -13,27 +13,25 @@ $adress =  get_field('contact')['adress'];
     <?php while (have_posts()) : the_post(); ?>
         <div class="row">
             <div class="col-md-12 contact-map">
-            <iframe src="<?php echo $map_url ?>" width="100%" height="350" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-            </div>
-            <div class="col-md-12">
-            <div class="contact-container">
-                <p><?php echo $adress ?></p>
-            </div>
+                <iframe src="<?php echo $map_url ?>" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                <div class="contact-adress-container">
+                    <p><?php echo $adress ?></p>
+                </div>
             </div>
             <div class="col-md-12 single-title-container">
                 <h3 class="single-title"><?php the_title(); ?></h3>
             </div>
         </div>
         <section class="single-page-container row">
-          
-        
+
+
         </section>
-        <?php endwhile;  ?>
+    <?php endwhile;  ?>
 
-    <?php else : ?>
-        <!-- show 404 error here -->
-    <?php endif; ?>
+<?php else : ?>
+    <!-- show 404 error here -->
+<?php endif; ?>
 
-    <?php
-    wp_reset_postdata();
-    get_footer(); ?>
+<?php
+wp_reset_postdata();
+get_footer(); ?>
